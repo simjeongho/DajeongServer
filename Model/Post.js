ceb8cc58abd9dbd2037456345a915d61deec817d
simjeongho <simjeongho012@gmail.com>
2022-07-02 16:14:10 +0900

    feat: express에서 ES6 router사용

--- Model/Post.js
+++ Model/Post.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import express from "express";
+export const Postsrouter = express.Router();
 const postSchema = new mongoose.Schema({
 	title: String,
 	content: String,
