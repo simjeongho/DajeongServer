19780dcf884f689c890a34b4fced8fc7bd8e0de6
simjeongho <simjeongho012@gmail.com>
2022-07-05 14:08:29 +0900

    refactor: singleAlbum Api 수정

--- Model/singleAlbum.js
+++ Model/singleAlbum.js
@@ -1,32 +1,62 @@
 import mongoose from "mongoose";
 import express from "express";
 import uploadImage from "../multer.js";
+import Counter from "./counter.js";
+import cors from "cors";
 export const singleAlbumRouter = express.Router();
 const singleAlbumSchema = new mongoose.Schema({
 	title: String,
 	content: String,
+	filePath: String,
+	postNum: Number,
 });
 
 const SingleAlbum = mongoose.model("singleAlbum", singleAlbumSchema);
 
-singleAlbumRouter.post("/uploadImage", uploadImage.single("singleImage"), (req, res, next) => {
+singleAlbumRouter.post("/uploadSingleAlbumPost", cors(), uploadImage.single("singleImage"), (req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
 	if (next) {
+		const imageUrl = "http://localhost:5000/public/singleAlbum";
 		const body = req.body;
-		console.log(body);
-		console.log(req.file);
-		res.status(200).json({ success: true, filePath: res.req.file.path });
-		const singleAlbum = new SingleAlbum(body);
-		singleAlbum
-			.save()
-			.then(() => {
-				res.status(200).json({ success: true, text: "몽고DB저장 성공" });
-			})
-			.catch((err) => {
-				res.status(400).json({ success: false });
+		Counter.findOne({ name: "counter" })
+			.exec()
+			.then((counter) => {
+				body.postNum = counter.postNum;
+				console.log(body);
+				console.log(req.file);
+
+				const singleAlbum = new SingleAlbum({ ...body, filePath: res.req.file.path });
+				singleAlbum
+					.save()
+					.then(() => {
+						Counter.updateOne({ name: "counter" }, { $inc: { postNum: 1 } }).then(() => {
+							res.setHeader("Access-Control-Allow-Origin", "*");
+
+							res
+								.status(200)
+								.json({ success: true, filePath: `${imageUrl}${res.req.file.path}`, text: "몽고 DB 저장 성공" });
+						});
+					})
+					.catch((err) => {
+						res.status(400).json({ success: false });
+					});
 			});
 	} else {
 		res.send("SingleImageTestSuccess!");
 	}
+});
+
+singleAlbumRouter.get("/getList", (req, res, next) => {
+	SingleAlbum.find()
+		.exec()
+		.then((doc) => {
+			res.setHeader("Access-Control-Allow-Origin", "*");
+			res.setHeader("X-Content-Type-Options", "nosniff");
+			res.status(200).json({ success: true, singleAlbumList: doc });
+		})
+		.catch((err) => {
+			res.status(400).json({ success: false });
+		});
 });
 
 export default SingleAlbum;
