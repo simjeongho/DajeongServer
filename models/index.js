4c960000f8f77bc2d544e4780de52c94ecb73c08
simjeongho <simjeongho012@gmail.com>
2022-07-08 11:16:49 +0900

    feat: mysql sequelize 연결

---
+++ models/index.js
@@ -0,0 +1,16 @@
+import Sequelize from "sequelize";
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config.json")[env];
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+Object.keys(db).forEach((modelName) => {
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
