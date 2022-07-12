b1dcfe71da93d9b099d41d184891a1d37eb7941e
simjeongho <simjeongho012@gmail.com>
2022-07-12 15:13:30 +0900

    feat: logout라우터

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,14 @@
-import express from "express";
-import passport from "passport";
+import express from 'express';
+import next from 'next';
+import passport from 'passport';
+import db from '../models/index.js';
 export const userRouter = express.Router();
 
-userRouter.post("/login", (req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+const User = db.User;
+userRouter.post('/login', (req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
 	console.log(req);
-	passport.authenticate("local", (err, user, info) => {
-		res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+	passport.authenticate('local', (err, user, info) => {
 		if (err) {
 			console.error(err);
 			return next(err);
@@ -15,14 +17,37 @@ userRouter.post("/login", (req, res, next) => {
 			return res.status(401).send(info.reason);
 		}
 		return req.login(user, async (loginErr) => {
-			res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
 			if (loginErr) {
 				console.log(loginErr);
 				return next(loginErr);
 			}
-			return res.json(user);
+			const fullUserWithoutPassword = await User.findOne({
+				where: { id: user.id },
+				attributes: {
+					exclude: ['password'],
+				},
+				include: [
+					{
+						model: db.Post,
+					},
+					{
+						model: db.Comment,
+					},
+				],
+			});
+			return res.status(200).json(fullUserWithoutPassword);
 		});
 	})(req, res, next);
+});
+
+userRouter.post('/logout', (req, res) => {
+	req.logout(function (err) {
+		if (err) {
+			return next(err);
+		}
+		req.session.destroy();
+	});
+	res.send('logout Success');
 });
 
 export default userRouter;
