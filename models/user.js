2d6406a2e1c47daff5daa39cd698ef65317e8eca
simjeongho <simjeongho012@gmail.com>
2022-07-28 22:45:58 +0900

    fix: user table column 추가

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 export const User = (sequelize, DataTypes) => {
 	const User = sequelize.define(
-		"User",
+		'User',
 		{
 			//id가 기본적으로 들어있다.
 			email: {
@@ -16,18 +16,24 @@ export const User = (sequelize, DataTypes) => {
 				type: DataTypes.STRING(100),
 				allowNull: false,
 			},
+			profileImage: {
+				type: DataTypes.STRING(200),
+			},
+			profileDescription: {
+				type: DataTypes.STRING(300),
+			},
 		},
 		{
-			charset: "utf8",
-			collate: "utf8_general_ci", // 한글 저장
+			charset: 'utf8',
+			collate: 'utf8_general_ci', // 한글 저장
 		},
 	);
 	User.associate = (db) => {
 		db.User.hasMany(db.Post);
 		db.User.hasMany(db.Comment);
-		db.User.belongsToMany(db.Post, { through: "Like", as: "Liked" });
-		db.User.belongsToMany(db.User, { through: "Follow", as: "Followers", foreignKey: "followingId" });
-		db.User.belongsToMany(db.User, { through: "Follow", as: "Followings", foreignKey: "followerId" });
+		db.User.belongsToMany(db.Post, { through: 'Like', as: 'Liked' });
+		db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followers', foreignKey: 'followingId' });
+		db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followings', foreignKey: 'followerId' });
 	};
 	return User;
 };
